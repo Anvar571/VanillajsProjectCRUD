f0c64235bb713fca20504dde356f73b648f5aae5
Anvar571 <abdurashidovanvar52@gmail.com>
2023-01-22 00:28:13 +0500

    Js Simple Spa app

---
+++ app.js
@@ -0,0 +1,12 @@
+const createBtn = document.getElementById("addProduct")
+// create product
+// import CreateProduct from "./products/create-product";
+
+// delete product
+// import DeleteProduct from "./products/delete-product";
+
+// read product
+// import ReadProduct from "./products/read-product";
+
+// update product
+// import UpdateProduct from "./products/update-product";
